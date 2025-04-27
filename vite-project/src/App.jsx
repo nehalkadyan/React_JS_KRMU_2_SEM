6c63a7066e30a63d1de7eb82403c92fa2a72356d
nehal <kadyannehal333@gmail.com>
2025-04-27 10:31:52 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+// import Navbar from "./components/Navbar";
+// import Section from "./components/Section";
+// import Footer from "./components/Footer";
+import { useState } from "react";
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  console.log("count : ",count)
+
+  return <div>
+    {/* <Navbar />
+    <Section />
+    <Footer /> */}
+  </div>;
+}
+
+export default App;
